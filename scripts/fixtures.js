eb66ffea02e01662a435c2a925e85e40a189a4d4
anthony zardis <your>
2017-03-16 10:10:31 -0700

    checkpoint-19-JQ-next/previous

---
+++ scripts/fixtures.js
@@ -0,0 +1,45 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        {title: 'Blue', duration: '4:26' },
+        {title: 'Green', duration: '3:14' },
+        {title: 'Red', duration: '5:01' },
+        {title: 'Pink', duration: '3:21' },
+        {title: 'Magenta', duration: '2:15' }
+        
+    ]
+};
+
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21' },
+        { title: 'Can you hear me now', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15' }      
+    ]
+};
+
+var albumDreDog = {
+    title: 'The New Jim Jones',
+    artist: 'Dre Dog',
+    label: 'In-a-Minute Records',
+    year: '1993',
+    albumArtUrl: 'assets/images/album_covers/The_New_Jim_Jones.jpg',
+    songs: [
+        { title: 'Lets Get High', duration: '1:24' },
+        { title: 'Most Hated Man in Frisco', duration: '3:08' },
+        { title: 'The Ave.', duration: '4:27' },
+        { title: 'Smoke Dope & Rap', duration: '4:36' },
+        { title: 'Off That Chewy', duration: '3:46'}      
+    ]
+};
