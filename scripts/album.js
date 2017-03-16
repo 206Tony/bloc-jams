b533930ebcecce93543ec2013a02de7d8fbcb52c
anthony zardis <your>
2017-03-16 10:51:14 -0700

    checkpoint-19-JQ-next/previous

--- scripts/album.js
+++ scripts/album.js
@@ -1,3 +1,14 @@
+var setSong = function (songNumber) {
+    currentlyPlayingSongNumber = parseInt(songNumber);
+    currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+};
+
+var getSongNumberCell = function(number) {
+    
+    return $('song-item-number[data-song-number="' + number + '"]');
+};
+
+
 var createSongRow = function (songNumber, songName, songLength) {
     var template =
             '<tr class="album-view-song-item">'
@@ -13,13 +24,13 @@ var createSongRow = function (songNumber, songName, songLength) {
         var songNumber = parseInt($(this).attr('data-song-number'));
         
         if (currentlyPlayingSongNumber !== null) {
-            var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+            var currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
             currentlyPlayingCell.html(currentlyPlayingSongNumber);
         }
         
         if (currentlyPlayingSongNumber !== songNumber) {
             $(this).html(pauseButtonTemplate);
-            currentlyPlayingSongNumber = songNumber;
+            setSong (songNumber);
             currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
             updatePlayerBarSong();
 
